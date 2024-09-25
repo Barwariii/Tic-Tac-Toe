6ebbd658808ec69da9dfc3c86816e608a6ba9c44
Bilal Mohammed <kurdo1ka@gmail.com>
2024-09-25 16:06:01 +0200

    maked array

---
+++ script.js
@@ -0,0 +1,11 @@
+let fields = [
+    'circle',  // 0
+    'circle',  // 1
+    'circle',  // 2
+    null,      // 3
+    'cross',   // 4
+    null,      // 5
+    'cross',   // 6
+    null,      // 7
+    null,      // 8
+]
