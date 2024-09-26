b9efbcafad3bb7045da3855de1cb4d0392996d8f
Bilal Mohammed <kurdo1ka@gmail.com>
2024-09-26 15:44:13 +0200

    first design & edited js code.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let fields = [
     null, // 0
-    null, // 1
-    null, // 2
+    'circle', // 1
+    'cross', // 2
     null, // 3
     null, // 4
     null, // 5
@@ -9,3 +9,44 @@ let fields = [
     null, // 7
     null, // 8
 ]
+
+
+// Definition der Symbole
+const circle = 'O';
+const cross = 'X';
+
+
+function init() {
+    render();
+}
+
+
+// Die Render-Funktion
+function render() {
+    const contentDiv = document.getElementById('content');
+    let tableHTML = '<table>';
+
+    for (let row = 0; row < 3; row++) {
+        tableHTML += '<tr>';
+        for (let col = 0; col < 3; col++) {
+            const index = row * 3 + col;
+            const cellValue = fields[index];
+            let displayValue = '';
+
+            // Einfache if-Abfragen zur Darstellung der Symbole
+            if (cellValue === 'circle') {
+                displayValue = 'O';
+            } else if (cellValue === 'cross') {
+                displayValue = 'X';
+            } else {
+                displayValue = ''; // Für leere Felder
+            }
+
+            tableHTML += `<td>${displayValue}</td>`;
+        }
+        tableHTML += '</tr>';
+    }
+
+    tableHTML += '</table>';
+    contentDiv.innerHTML = tableHTML;
+}
